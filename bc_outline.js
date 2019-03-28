d3407462a5230b212c0309033c3a40c4cc1d4e37
Unknown <pcapuano978@west-mec.org>
2019-03-28 14:42:58 -0700

    Exercise Started for the New Chapter
    Nothing too much done besides normal coding procedures, as we learned about nodes and node trees, although, I have happily except my new part in reminding Clawson to get some Socrative questions up in running at relevant times.

--- bc_outline.js
+++ bc_outline.js
@@ -5,8 +5,8 @@
    Tutorial 12
    Tutorial Case
 
-   Author: 
-   Date:   
+   Author: Patrick M. Capuano
+   Date:   3.28.19
 
    Filename: bc_outline.js
 
@@ -27,3 +27,16 @@
 
 */
 
+// Generate an outline based on h1 through h6 headings in the source document.
+window.addEventListener("load", makeOutLine);
+
+
+function makeOutLine() {
+
+      // Location of the outline.
+      var outline = document.getElementById("outline");
+
+      // Source document for the outline.
+      var source = document.getElementById("doc");
+
+}
